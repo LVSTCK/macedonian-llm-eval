e6a6b57254c33abee35d422975419192678dd55a
Stefan Krsteski <58995762+Stefanstud@users.noreply.github.com>
2025-01-05 18:51:09 +0100

    Delete tes.py

--- tes.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-# Load the dataset for the 'piqa' task
-dataset = load_dataset("LVSTCK/macedonian-llm-eval", name="boolq")
-
-# Print the dataset to confirm it loads
-print(dataset)
